ad10997bde46cbfe100b8bfc90aef01d622372c2
Michael Yang <themichaelyang@gmail.com>
2025-09-16 23:38:48 -0400

    fixed cosine sine, thanks Austin

--- vines/index.js
+++ vines/index.js
@@ -17,48 +17,31 @@ window.onload = () => {
   let center = [0, 0]
   let radius = 10
 
-  arcWalk([100, 100], 0, [25, 10, 8, 5, 3, 2], ctx)
+  wiggleLine([100, 100], Math.PI, [25, 10, 8, 5, 3, 2], ctx)
 }
 
-// start = starting point *on* a circle
-// startAngle = angle from starting point to first center
-function arcWalk(start, startAngle, radii, ctx) {
-  let [x, y] = start
-  let [cx, cy] = moveInDirection(start, radii[0], startAngle)
-  let turn = remap(Math.random(), Math.PI / 8, Math.PI)
+// // start = starting center point of a circle
+// // startAngle = end angle of starting semicircle
+// function arcWalk(start, startAngle, radii, ctx) {
+//   let [x, y] = start
+//   let turn = Math.PI
+//   let clockwise = true
 
-  ctx.beginPath()
-  ctx.fillStyle = 'red'
-  ctx.arc(x, y, 1, 0, 2 * Math.PI)
-  ctx.fill()
+//   drawPoint(x, y, ctx)
 
-  for (let i = 0; i < radii.length; i++) {
-    ctx.beginPath()
-    ctx.fillStyle = 'red'
-    ctx.arc(cx, cy, 1, 0, 2 * Math.PI)
-    ctx.fill()
+//   for (let i = 0; i < radii.length; i++) {
 
-    ctx.beginPath()
-    let arcStart = cartesianToPolar([x, y], [cx, cy])
-
-    ;[x, y] = moveInDirection([cx, cy], radii[i], turn)
-    let arcEnd = cartesianToPolar([x, y], [cx, cy])
-
-    turn = remap(Math.random(), 0.1 * Math.PI, Math.PI)
-    ctx.arc(cx, cy, radii[i], arcStart[1], arcEnd[1])
-    ;[cx, cy] = moveInDirection([x, y], radii[i + 1], startAngle)
-
-    ctx.stroke()
-  }
-}
+//   }
+// }
 
 // start = starting center point
 function wiggleLine(start, angle, radii, ctx) {
   let [x, y] = start
   for (let i = 0; i < radii.length; i++) {
+    ctx.beginPath()
     ctx.arc(x, y, radii[i], angle, angle + Math.PI, i % 2)
-    x += (radii[i] + radii[i + 1]) * Math.sin(angle)
-    y += (radii[i] + radii[i + 1]) * Math.cos(angle)
+    x += (radii[i] + radii[i + 1]) * Math.cos(angle)
+    y += (radii[i] + radii[i + 1]) * Math.sin(angle)
     ctx.stroke()
   }
 }
@@ -68,14 +51,14 @@ function circleLine(start, angle, radii, ctx) {
   for (let i = 0; i < radii.length; i++) {
     ctx.moveTo(x + radii[i], y)
     ctx.arc(x, y, radii[i], 0, 2 * Math.PI)
-    x += (radii[i] + radii[i + 1]) * Math.sin(angle)
-    y += (radii[i] + radii[i + 1]) * Math.cos(angle)
+    x += (radii[i] + radii[i + 1]) * Math.cos(angle)
+    y += (radii[i] + radii[i + 1]) * Math.sin(angle)
   }
   ctx.stroke()
 }
 
 function polarToCartesian(radius, angle, origin=[0, 0]) {
-  return [r * Math.sin(angle) + origin[0], r * Math.cos(angle) + origin[1]]
+  return [r * Math.cos(angle) + origin[0], r * Math.sin(angle) + origin[1]]
 }
 
 function cartesianToPolar(coord, origin=[0, 0]) {
@@ -93,4 +76,11 @@ function moveInDirection(start, length, angle) {
 // normalized: between 0 and 1
 function remap(normalized, max, min) {
   return (normalized * (max - min)) * max
+}
+
+function drawPoint(x, y, ctx) {
+  ctx.beginPath()
+  ctx.fillStyle = 'red'
+  ctx.arc(x, y, 1, 0, 2 * Math.PI)
+  ctx.fill()
 }
